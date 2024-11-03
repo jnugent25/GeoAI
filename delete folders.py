3de6fbfdea95391e0cd02fa6affa9db04599bb93
Jack Nugent <83517345+daawesomeguy@users.noreply.github.com>
2024-11-03 18:26:55 -0500

    Update and rename del.py to delete folders.py

--- delete folders.py
+++ delete folders.py
@@ -4,6 +4,5 @@ root = "C:\\Users\\Jack\\Downloads\\data\\train"
 folders = list(os.walk(root))[1:]
 
 for folder in folders:
-    # folder example: ('FOLDER/3', [], ['file'])
     if not folder[2]:
         os.rmdir(folder[0])
